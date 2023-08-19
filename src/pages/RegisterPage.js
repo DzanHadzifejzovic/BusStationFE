3894b14ccc45be403bdea5ad8a4da5b47cc21cd8
Dzan Hadzifejzovic <dzanhadzifejzovic123@gmail.com>
2023-08-19 12:50:19 +0200

    change file location for page Register

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import Register from '../components/Register/index'
+import React from 'react';
+import { Label, Wrapper } from '../components/Register/Register.styles';
+import Button from '../components/Button';
+import RegisterForm from '../components/Register/RegisterForm';
+
 
 const RegisterPage = () => {
   return (
-    <Register></Register>
-  )
-}
+    <>
+      <Wrapper>
+        <Label> Please, register first </Label>
+        <Button location='/register-page'></Button>
+      </Wrapper>
+      <RegisterForm></RegisterForm>
+    </>
+    
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;

685e1470574721d5fb113499f95e96729ad7d24a
Dzan Hadzifejzovic <dzanhadzifejzovic123@gmail.com>
2023-08-19 12:49:10 +0200

    change file location for page Login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import Login from '../components/Login/index';
+import React from 'react';
+import { Label, Wrapper } from '../components/Login/Login.styles';
+import LoginForm from '../components/Login/LoginForm';
 
 const LoginPage = () => {
   return (
-    <Login></Login>
-  )
-}
+    <>
+      <Wrapper>
+        <Label> Please, login first</Label>
+      </Wrapper>
+      <LoginForm></LoginForm>
+    </>
+    
+  );
+};
 
 export default LoginPage;
